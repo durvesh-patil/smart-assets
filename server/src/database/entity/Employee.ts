19fd867b21210449e622f0afcf4943c2773a8745
DrejT <vivekpatel4049@gmail.com>
2024-09-29 00:35:33 +0530

    added seed script and asset model

--- server/src/database/entity/Employee.ts
+++ server/src/database/entity/Employee.ts
@@ -1,33 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  employeeNo: number;
 
-    @Column()
-    employeeNo: number
+  @Column()
+  fullName: string;
 
-    @Column()
-    fullName: string
+  @Column()
+  joiningDate: string;
 
-    @Column()
-    joiningDate: string
+  @Column({ default: "N/A" })
+  resignDate: string;
 
-    @Column({ default: "N/A" })
-    resignDate: string
+  @Column({ default: "N/A" })
+  department: string;
 
-    @Column()
-    department: string
+  @Column({ default: "6" })
+  floor: "5" | "6";
 
-    @Column()
-    floor: "5" | "6"
+  @Column({ default: "N/A" })
+  emailId: string;
 
-    @Column()
-    emailId: string
-
-    @Column({ default: "N/A" })
-    skypeId: string
-
+  @Column({ default: "N/A" })
+  skypeId: string;
 }
