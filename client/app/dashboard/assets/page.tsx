28be34dba76299e7cce7aaa17d91a072015ef7ea
DrejT <vivekpatel4049@gmail.com>
2024-10-27 19:17:04 +0530

    integrated apis to frontend

--- client/app/dashboard/assets/page.tsx
+++ client/app/dashboard/assets/page.tsx
@@ -1,9 +1,106 @@
 "use client";
 import useAssets from "@/hooks/useAssets";
-import { Box } from "@mui/material";
+import useAssetTemplates from "@/hooks/useAssetTemplates";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Assets() {
   const { assetsList } = useAssets();
-  console.log(assetsList);
-  return <Box>assets page</Box>;
+  const { assetTemplates } = useAssetTemplates(); // Fetch asset templates
+  const [open, setOpen] = useState(false);
+  const [selectedTemplate, setSelectedTemplate] = useState("");
+  const [templateFields, setTemplateFields] = useState([]);
+  const [formValues, setFormValues] = useState({}); // State to hold form field values
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedTemplate(""); // Reset selected template when closing
+    setTemplateFields([]); // Clear template fields
+    setFormValues({}); // Reset form values
+  };
+
+  const handleTemplateChange = (event) => {
+    const templateId = event.target.value;
+    setSelectedTemplate(templateId);
+
+    // Find the selected template and set its fields
+    const selectedAssetTemplate = assetTemplates.find(
+      (template) => template.id === templateId
+    );
+    if (selectedAssetTemplate) {
+      setTemplateFields(selectedAssetTemplate.fields);
+      setFormValues({}); // Reset form values for the new template
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name,value)
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value, // Update the specific field in the formValues state
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can handle form submission, e.g., send data to your API
+    console.log("Form Values: ", formValues);
+  };
+
+  return (
+    <Box>
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Add Asset
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add New Asset</DialogTitle>
+        <DialogContent>
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Asset Template</InputLabel>
+            <Select value={selectedTemplate} onChange={handleTemplateChange}>
+              {assetTemplates?.map((template) => (
+                <MenuItem key={template.id} value={template.id}>
+                  {template.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <form onSubmit={handleSubmit}>
+            {templateFields?.map((field, index) => (
+              <TextField
+                key={index}
+                label={field.label}
+                type={field.type}
+                fullWidth
+                margin="normal"
+                required={field.required}
+                name={field.label}
+                onChange={handleInputChange} // Handle change for inputs
+              />
+            ))}
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Submit
+            </Button>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <Box>assets page</Box>
+    </Box>
+  );
 }
--- client/app/dashboard/templates/page.tsx
+++ client/app/dashboard/templates/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useEffect } from "react";
 import {
   TextField,
   Button,
@@ -12,132 +12,255 @@ import {
   InputLabel,
   TextareaAutosize,
   Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Card,
+  CardContent,
+  Typography,
 } from "@mui/material";
 import axios from "axios";
 import { API_URL } from "@/lib/constants";
+import { Delete, Edit } from "@mui/icons-material";
+import useAssetTemplates from "@/hooks/useAssetTemplates";
 
 function CreateTemplateForm() {
-  const [template, setTemplate] = useState<object>({
+  const [template, setTemplate] = useState({
     name: "",
     note: "",
-    properties: [],
+    fields: [],
   });
+
+  // const [templates, setTemplates] = useState([]); // State to hold fetched templates
+  const [open, setOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false); // State to track if editing
+  const {
+    assetTemplates: templates,
+    handleDeleteTemplate,
+    setAssetTemplates: setTemplates,
+  } = useAssetTemplates();
+
+  useEffect(() => {
+    const fetchTemplates = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/templates`);
+        console.log(response);
+        setTemplates(response.data.assetTemplates); // Assuming the response has a `templates` array
+      } catch (error) {
+        console.error("Error fetching templates:", error);
+      }
+    };
+
+    fetchTemplates();
+  }, []);
 
   const addProperty = () => {
     setTemplate((prev) => ({
       ...prev,
-      properties: [
-        ...prev.properties,
+      fields: [
+        ...prev.fields,
         { label: "", type: "text", options: [], required: false },
       ],
     }));
   };
 
   const handlePropertyChange = (index, key, value) => {
-    const updatedProperties = [...template.properties];
-    updatedProperties[index][key] = value;
-    setTemplate((prev) => ({ ...prev, properties: updatedProperties }));
+    const updatedFields = [...template.fields];
+    updatedFields[index][key] = value;
+    setTemplate((prev) => ({ ...prev, fields: updatedFields }));
   };
 
-  const submitTemplate = async (e: FormEvent) => {
+  const submitTemplate = async (e) => {
     e.preventDefault();
-    console.log(template);
-    const res = await axios.post(API_URL + "/templates", template);
-    console.log(res);
-    // Handle submitting the template to the server
+    try {
+      if (isEditing) {
+        // Update template logic
+        const res = await axios.put(
+          `${API_URL}/templates/${template.id}`,
+          template
+        );
+        console.log(res);
+      } else {
+        // Create new template logic
+        const res = await axios.post(`${API_URL}/templates`, template);
+        console.log(res);
+      }
+      // Optionally, refetch templates after creating or updating one
+      const response = await axios.get(`${API_URL}/templates`);
+      setTemplates(response.data.templates);
+      handleClose(); // Close the dialog after submitting
+    } catch (error) {
+      console.error("Error creating/updating template:", error);
+    }
   };
 
+  const handleClickOpen = (selectedTemplate = null) => {
+    if (selectedTemplate) {
+      setTemplate(selectedTemplate);
+      setIsEditing(true);
+    } else {
+      setTemplate({ name: "", note: "", fields: [] }); // Reset for new template
+      setIsEditing(false);
+    }
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    // Reset template state when closing the dialog
+    setTemplate({ name: "", note: "", fields: [] });
+    setIsEditing(false); // Reset editing state
+  };
+
+  console.log(templates);
   return (
-    <form onSubmit={submitTemplate}>
+    <Box>
       <Box>
-        <TextField
-          label="Template Name"
-          fullWidth
-          value={template.name}
-          onChange={(e) => setTemplate({ ...template, name: e.target.value })}
-          margin="normal"
-        />
-        <TextareaAutosize
-          minRows={3}
-          style={{
-            border: "1px solid #ccc", // Set your desired border color and style
-            borderRadius: "4px", // Optional: Add border radius
-            padding: "8px", // Optional: Add some padding
-            width: "100%", // Optional: Set width
-            boxSizing: "border-box", // Optional: Ensure padding and border are included in width
-          }}
-          value={template.note}
-          onChange={(e) => setTemplate({ ...template, note: e.target.value })}
-          placeholder="Add a note"
-        />
-        {template.properties.map((property, index) => (
-          <Grid2 container spacing={2} key={index} alignItems="center">
-            <TextField
-              label="Property Label"
-              fullWidth
-              value={property.label}
-              onChange={(e) =>
-                handlePropertyChange(index, "label", e.target.value)
-              }
-            />
-            <FormControl fullWidth>
-              <InputLabel>Type</InputLabel>
-              <Select
-                value={property.type}
-                onChange={(e) =>
-                  handlePropertyChange(index, "type", e.target.value)
-                }
-              >
-                <MenuItem value="text">Text</MenuItem>
-                <MenuItem value="number">Number</MenuItem>
-                <MenuItem value="select">Select</MenuItem>
-              </Select>
-            </FormControl>
-            {property.type === "select" && (
-              <TextField
-                label="Options (comma-separated)"
-                fullWidth
-                onChange={(e) =>
-                  handlePropertyChange(
-                    index,
-                    "options",
-                    e.target.value.split(",")
-                  )
-                }
-              />
-            )}
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={property.required}
+        <Button variant="outlined" onClick={() => handleClickOpen()}>
+          Add Template
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle>
+            {isEditing ? "Update Template" : "Create New Template"}
+          </DialogTitle>
+          <DialogContent>
+            <form onSubmit={submitTemplate}>
+              <Box>
+                <TextField
+                  label="Template Name"
+                  fullWidth
+                  value={template.name}
                   onChange={(e) =>
-                    handlePropertyChange(index, "required", e.target.checked)
+                    setTemplate({ ...template, name: e.target.value })
                   }
+                  margin="normal"
                 />
-              }
-              label="Required"
-            />
-          </Grid2>
-        ))}
+
+                {template.fields?.map((property, index) => (
+                  <Grid2 container spacing={2} key={index} alignItems="center">
+                    <TextField
+                      label="Property Label"
+                      fullWidth
+                      value={property.label}
+                      onChange={(e) =>
+                        handlePropertyChange(index, "label", e.target.value)
+                      }
+                    />
+                    <FormControl fullWidth>
+                      <InputLabel>Type</InputLabel>
+                      <Select
+                        value={property.type}
+                        onChange={(e) =>
+                          handlePropertyChange(index, "type", e.target.value)
+                        }
+                      >
+                        <MenuItem value="text">Text</MenuItem>
+                        <MenuItem value="number">Number</MenuItem>
+                        <MenuItem value="select">Select</MenuItem>
+                      </Select>
+                    </FormControl>
+                    {property.type === "select" && (
+                      <TextField
+                        label="Options (comma-separated)"
+                        fullWidth
+                        value={property.options.join(", ")}
+                        onChange={(e) =>
+                          handlePropertyChange(
+                            index,
+                            "options",
+                            e.target.value.split(",")
+                          )
+                        }
+                      />
+                    )}
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={property.required}
+                          onChange={(e) =>
+                            handlePropertyChange(
+                              index,
+                              "required",
+                              e.target.checked
+                            )
+                          }
+                        />
+                      }
+                      label="Required"
+                    />
+                  </Grid2>
+                ))}
+                <TextareaAutosize
+                  minRows={3}
+                  style={{
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    padding: "8px",
+                    width: "100%",
+                    boxSizing: "border-box",
+                  }}
+                  value={template.note}
+                  onChange={(e) =>
+                    setTemplate({ ...template, note: e.target.value })
+                  }
+                  placeholder="Add a note"
+                />
+              </Box>
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={addProperty}
+                style={{ marginTop: "20px" }}
+              >
+                Add Property
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                style={{ marginTop: "20px", marginLeft: "10px" }}
+              >
+                {isEditing ? "Update Template" : "Create Template"}
+              </Button>
+            </form>
+          </DialogContent>
+        </Dialog>
       </Box>
 
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={addProperty}
-        style={{ marginTop: "20px" }}
-      >
-        Add Property
-      </Button>
-      <Button
-        type="submit"
-        variant="contained"
-        color="secondary"
-        style={{ marginTop: "20px", marginLeft: "10px" }}
-      >
-        Create Template
-      </Button>
-    </form>
+      {/* Display templates as cards */}
+      <Box mt={4} display="flex" flexDirection="column" gap={2}>
+        {templates?.map((template) => (
+          <Card key={template.id}>
+            <CardContent>
+              <Box
+                display={"flex"}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+              >
+                <Box pr={"20px"}>
+                  <Typography variant="h5">{template.name}</Typography>
+                  {/* <Typography variant="body1">{template.notes}</Typography> */}
+                </Box>
+                <Box>
+                  <Button onClick={() => handleClickOpen(template)}>
+                    <Edit />
+                  </Button>
+                  <Button onClick={() => handleDeleteTemplate(template.id)}>
+                    <Delete />
+                  </Button>
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
+    </Box>
   );
 }
 
